2d62758afdd43d1b68c4359f7b0c6783b2094862
方觉 (Fang Jue) <fangjue23303@gmail.com>
2023-12-08 08:24:09 +0800

    Update package name and application name.

--- buildSrc/src/main/kotlin/Build.kt
+++ buildSrc/src/main/kotlin/Build.kt
@@ -2,7 +2,7 @@
  * created by sca_tl at 2022/4/20 20:22
  */
 object BuildVersion {
-    const val applicationId     =       "com.scatl.uestcbbs"
+    const val applicationId     =       "com.scatl.uestcbbs.test2023"
     const val versionCode       =       322
     const val versionName       =       "3.1.7.1"
     const val buildToolsVersion =       "32.0.0"
